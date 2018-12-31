44c7b27da7789ab14d87ef0550a83bbcb3ddfa4c
Alex Barker <alexbark@gmail.com>
2018-12-31 14:10:18 +1100

    Added model details and mapper

---
+++ spotify-playlist-tracker.Worker/spotify-playlist-tracker.Worker/Mappers/CurrentlyPlayingContextToTrackEntity.cs
@@ -0,0 +1,26 @@
+﻿using spotify_playlist_tracker.Worker.Models.Storage;
+using SpotifyWebApi.Model;
+using System.Linq;
+
+namespace spotify_playlist_tracker.Worker.Mappers
+{
+    public static class CurrentlyPlayingContextToTrackEntity
+    {
+        public static TrackEntity Map(CurrentlyPlayingContext currentlyPlayingContext)
+        {
+            return new TrackEntity()
+            {
+                Name = currentlyPlayingContext.Item.Name,
+                Artists = currentlyPlayingContext.Item.Artists.Select(x => x.Name).ToList(),
+                Popularity = currentlyPlayingContext.Item.Popularity,
+                TrackLength = currentlyPlayingContext.Item.DurationMs,
+                Album = currentlyPlayingContext.Item.Album.Name,
+                // ReleaseDate = currentlyPlayingContext.Item.Album,
+                RetrievedTimestamp = currentlyPlayingContext.Timestamp,
+                AlbumArtHeight = currentlyPlayingContext.Item.Album.Images.FirstOrDefault().Height,
+                AlbumArtWidth = currentlyPlayingContext.Item.Album.Images.FirstOrDefault().Width,
+                AlbumArtUrl = currentlyPlayingContext.Item.Album.Images.FirstOrDefault().Url
+            };
+        }
+    }
+}
--- spotify-playlist-tracker.Worker/spotify-playlist-tracker.Worker/Models/Storage/TrackEntity.cs
+++ spotify-playlist-tracker.Worker/spotify-playlist-tracker.Worker/Models/Storage/TrackEntity.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
 
 namespace spotify_playlist_tracker.Worker.Models.Storage
 {
     public class TrackEntity : TableEntity
     {
         public string Name { get; set; }
-        public string Artist { get; set; }
+        public List<string> Artists { get; set; }
+        public int Popularity { get; set; }
+        public int TrackLength { get; set; }
+        public string Album { get; set; }
+        public string ReleaseDate { get; set; }
+        public DateTime RetrievedTimestamp { get; set; }
+
+        public int? AlbumArtHeight { get; set; }
+        public int? AlbumArtWidth { get; set; }
+        public string AlbumArtUrl { get; set; }
+
     }
 }
